fd2368880b0d92d627222b7642d28b28433c7cf7
felipeluz99 <116134526+felipeluz99@users.noreply.github.com>
2024-10-26 20:09:55 -0300

    Update folha.py

--- ranking_folha/folha.py
+++ ranking_folha/folha.py
@@ -1,19 +1,41 @@
 import pandas as pd
 import random
 
+
 def df_universidades():
     df = pd.read_csv("ranking_folha_univ.csv", delimiter=';')
-    df = df[["Universidade", "Estado", "Pública ou Privada", "Nota em Ensino", "Nota em Internacionalização", "Nota em Mercado",
-             "Nota em Inovação"]]
+    df = df[["Universidade", "Estado", "Pública ou Privada", "Nota em Ensino", "Nota em Internacionalização",
+             "Nota em Mercado", "Nota em Inovação"]]
+
     ids = random.sample(range(1, 204), len(df))
     df["Código MEC"] = ids
 
+    org = ['Faculdade', 'Centro Universitário', 'Institutos Federais', 'Universidade', 'Escola de Governo']
     with open("inserts_universidades.txt", "w", encoding="utf-8") as f:
         for _, linha in df.iterrows():
-            sql = f"INSERT INTO ies (codigoemecies, nome, tipo, notaensinoruf,notrufinovacao, notarufmercado, " \
-                  f"notarufinternacionalizacao)" f"VALUES ('{linha['Código MEC']},{linha['Universidade']},{linha['Pública ou Privada']},{linha['Nota em Ensino']},{linha['Nota em Inovação']},{linha['Nota em Mercado']}',{linha['Nota em Internacionalização']});\n "
-            f.write(sql)
-    return df
 
+            igc = round(random.uniform(1, 5), 2)
+            ci_ead = round(random.uniform(1, 5), 2)
+            ci = round(random.uniform(1, 5), 2)
+            npcs = random.randint(3000, 4000)
+            nlabs = random.randint(1, 20)
+            nlivros = random.randint(10000, 20000)
+            nalunos = random.randint(5000, 30000)
+            org_random = random.choice(org)
+            localizacao = linha['Estado']
+
+            sql = (
+                f"INSERT INTO ies (codigoemecies, nlivrosbiblioteca, npcsdisponiveis, nlabs, nalunos, ci, igc, "
+                f"ci_ead, tipo, nome, orgacademica, notaensinoruf, notrufinovacao, notarufmercado, "
+                f"notarufinternacionalizacao, localizacao) "
+                f"VALUES ({linha['Código MEC']}, {nlivros}, {npcs}, {nlabs}, {nalunos}, {ci}, {igc}, {ci_ead}, "
+                f"'{linha['Pública ou Privada']}', '{linha['Universidade']}', '{org_random}', "
+                f"{linha['Nota em Ensino']}, {linha['Nota em Inovação']}, {linha['Nota em Mercado']}, "
+                f"{linha['Nota em Internacionalização']}, '{localizacao}');\n"
+            )
+
+            f.write(sql)
+
+    return df
 
 df_universidades()
