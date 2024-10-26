f6d2ab3535ebebd38065791d5b00dac6a6e2d39d
felipeluz99 <116134526+felipeluz99@users.noreply.github.com>
2024-10-26 18:55:40 -0300

    Update folha.py

--- ranking_folha/folha.py
+++ ranking_folha/folha.py
@@ -3,15 +3,15 @@ import random
 
 def df_universidades():
     df = pd.read_csv("ranking_folha_univ.csv", delimiter=';')
-    df = df[["Universidade", "Estado", "Pública ou Privada", "Nota em Ensino", "Nota em Pesquisa", "Nota em Mercado",
+    df = df[["Universidade", "Estado", "Pública ou Privada", "Nota em Ensino", "Nota em Internacionalização", "Nota em Mercado",
              "Nota em Inovação"]]
     ids = random.sample(range(1, 204), len(df))
     df["Código MEC"] = ids
 
-    with open("universidades_sql.txt", "w", encoding="utf-8") as f:
+    with open("inserts_universidades.txt", "w", encoding="utf-8") as f:
         for _, linha in df.iterrows():
-            sql = f"INSERT INTO IES (Código MEC, Nome, Estado, Tipo, Nota Ensino, Nota Pesquisa, Nota Mercado, " \
-                  f"Nota Inovação) VALUES ('{linha['Código MEC']},{linha['Universidade']},{linha['Estado']},{linha['Pública ou Privada']},{linha['Nota em Ensino']},{linha['Nota em Pesquisa']},{linha['Nota em Mercado']}',{linha['Nota em Inovação']});\n "
+            sql = f"INSERT INTO ies (codigoemecies, nome, tipo, notaensinoruf,notrufinovacao, notarufmercado, " \
+                  f"notarufinternacionalizacao)" f"VALUES ('{linha['Código MEC']},{linha['Universidade']},{linha['Pública ou Privada']},{linha['Nota em Ensino']},{linha['Nota em Inovação']},{linha['Nota em Mercado']}',{linha['Nota em Internacionalização']});\n "
             f.write(sql)
     return df
 
