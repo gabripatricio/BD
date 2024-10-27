c06cd354e17bd9db17da49e60fdd2b916847c74c
felipeluz99 <116134526+felipeluz99@users.noreply.github.com>
2024-10-26 21:40:46 -0300

    Update folha.py

--- ranking_folha/folha.py
+++ ranking_folha/folha.py
@@ -13,7 +13,6 @@ def df_universidades():
     org = ['Faculdade', 'Centro Universitário', 'Institutos Federais', 'Universidade', 'Escola de Governo']
     with open("inserts_universidades.txt", "w", encoding="utf-8") as f:
         for _, linha in df.iterrows():
-
             igc = round(random.uniform(1, 5), 2)
             ci_ead = round(random.uniform(1, 5), 2)
             ci = round(random.uniform(1, 5), 2)
@@ -24,14 +23,19 @@ def df_universidades():
             org_random = random.choice(org)
             localizacao = linha['Estado']
 
+            # Função para converter NaN para NULL no SQL
+            def format_value(value):
+                return 'NULL' if pd.isna(value) else value
+
             sql = (
                 f"INSERT INTO ies (codigoemecies, nlivrosbiblioteca, npcsdisponiveis, nlabs, nalunos, ci, igc, "
                 f"ci_ead, tipo, nome, orgacademica, notaensinoruf, notrufinovacao, notarufmercado, "
                 f"notarufinternacionalizacao, localizacao) "
                 f"VALUES ({linha['Código MEC']}, {nlivros}, {npcs}, {nlabs}, {nalunos}, {ci}, {igc}, {ci_ead}, "
                 f"'{linha['Pública ou Privada']}', '{linha['Universidade']}', '{org_random}', "
-                f"{linha['Nota em Ensino']}, {linha['Nota em Inovação']}, {linha['Nota em Mercado']}, "
-                f"{linha['Nota em Internacionalização']}, '{localizacao}');\n"
+                f"{format_value(linha['Nota em Ensino'])}, {format_value(linha['Nota em Inovação'])}, "
+                f"{format_value(linha['Nota em Mercado'])}, {format_value(linha['Nota em Internacionalização'])}, "
+                f"'{localizacao}');\n"
             )
 
             f.write(sql)
